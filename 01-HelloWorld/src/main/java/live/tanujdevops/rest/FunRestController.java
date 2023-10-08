c8e3de2cb3496ba6e5d0d897284bc225839ccb7b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-08 06:44:32 +0530

    added endpoint for devtools auto restart feature, use only in dev env

--- 01-HelloWorld/src/main/java/live/tanujdevops/rest/FunRestController.java
+++ 01-HelloWorld/src/main/java/live/tanujdevops/rest/FunRestController.java
@@ -5,9 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
-//    expose "/" endpoint for home
+    //    expose "/" endpoint for home
     @GetMapping("/")
-    public String sayHello(){
+    public String sayHello() {
         return "Hello, World!";
+    }
+
+    //    expose endpoint for "workout"
+    @GetMapping("workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
     }
 }
