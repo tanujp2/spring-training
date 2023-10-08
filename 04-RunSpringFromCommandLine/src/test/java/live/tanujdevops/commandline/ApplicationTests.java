86432a7b474773da211e1f9c9edad44b2bcaea61
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-08 08:58:08 +0530

    added initial files

---
+++ 04-RunSpringFromCommandLine/src/test/java/live/tanujdevops/commandline/ApplicationTests.java
@@ -0,0 +1,13 @@
+package live.tanujdevops.commandline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
