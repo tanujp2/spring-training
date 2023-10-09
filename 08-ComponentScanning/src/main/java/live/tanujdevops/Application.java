e48249eecc77f32cc3258ad130f9db3f2ade886f
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-09 15:16:46 +0530

    fixed the component scanning packages

--- 08-ComponentScanning/src/main/java/live/tanujdevops/Application.java
+++ 08-ComponentScanning/src/main/java/live/tanujdevops/Application.java
@@ -3,7 +3,11 @@ package live.tanujdevops;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//Tell spring to scan additional packages for component
+//use scanBasePackages argument of @SpringBootApplication
+//do list the package where @SpringBootApplication resides also
+
+@SpringBootApplication(scanBasePackages = { "live.tanujdevops", "util" })
 public class Application {
 
 	public static void main(String[] args) {
