a6e41af3747fb7ad7a5f15cd337fe129376cce9b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-09 17:00:54 +0530

    added initial files

---
+++ 12-Primary/src/main/java/live/tanujdevops/common/TennisCoach.java
@@ -0,0 +1,13 @@
+package live.tanujdevops.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
