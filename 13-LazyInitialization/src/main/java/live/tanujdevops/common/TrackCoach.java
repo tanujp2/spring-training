9454b2a4b1f402b4c4a6c04b21dd746b70b58ad7
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 15:42:07 +0530

    updated files: lazy initialization at class level

--- 13-LazyInitialization/src/main/java/live/tanujdevops/common/TrackCoach.java
+++ 13-LazyInitialization/src/main/java/live/tanujdevops/common/TrackCoach.java
@@ -1,8 +1,10 @@
 package live.tanujdevops.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TrackCoach implements Coach {
 
 	public TrackCoach() {
