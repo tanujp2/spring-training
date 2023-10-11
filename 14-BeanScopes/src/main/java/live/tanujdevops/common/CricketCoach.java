eb1386399f9da46b46f1b3c0cf1b71b2f8240fb9
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 15:58:53 +0530

    updated files: for prototype scoped beans, each time a dependency is to be injected a new object is created

--- 14-BeanScopes/src/main/java/live/tanujdevops/common/CricketCoach.java
+++ 14-BeanScopes/src/main/java/live/tanujdevops/common/CricketCoach.java
@@ -1,8 +1,10 @@
 package live.tanujdevops.common;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class CricketCoach implements Coach {
 
 	public CricketCoach() {
