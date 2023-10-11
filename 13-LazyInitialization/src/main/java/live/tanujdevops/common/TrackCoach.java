6abcc4de8c17b1c2f0278cd7068d0208697dcc92
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-11 15:35:01 +0530

    added files

---
+++ 13-LazyInitialization/src/main/java/live/tanujdevops/common/TrackCoach.java
@@ -0,0 +1,13 @@
+package live.tanujdevops.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Run a hard 5k!";
+	}
+
+}
---
+++ 13-LazyInitialization/src/main/java/live/tanujdevops/rest/DemoRestController.java
@@ -0,0 +1,24 @@
+package live.tanujdevops.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import live.tanujdevops.common.Coach;
+
+@RestController
+public class DemoRestController {
+
+	private Coach coach;
+
+	@Autowired
+	public DemoRestController(@Qualifier("cricketCoach") Coach coach) {
+		this.coach = coach;
+	}
+
+	@GetMapping("/dailyworkout")
+	public String getDailyWorkout() {
+		return coach.getDailyWorkout();
+	}
+}
