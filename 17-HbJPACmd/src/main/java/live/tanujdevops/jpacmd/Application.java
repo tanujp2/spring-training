3b1848942f4c103e73222a7455f1dbe02f68c4b5
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-12 16:21:09 +0530

    added logic to create command lien runner bena

--- 17-HbJPACmd/src/main/java/live/tanujdevops/jpacmd/Application.java
+++ 17-HbJPACmd/src/main/java/live/tanujdevops/jpacmd/Application.java
@@ -1,7 +1,9 @@
 package live.tanujdevops.jpacmd;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +12,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello World!");
+		};
+	}
 }
