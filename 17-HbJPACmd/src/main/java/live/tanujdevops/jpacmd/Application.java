627212f1c87f1aa9cc9173850f1f3bc4a4a071dc
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-12 17:12:41 +0530

    updated files: add a new student

--- 17-HbJPACmd/src/main/java/live/tanujdevops/jpacmd/Application.java
+++ 17-HbJPACmd/src/main/java/live/tanujdevops/jpacmd/Application.java
@@ -1,12 +1,23 @@
 package live.tanujdevops.jpacmd;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import live.tanujdevops.jpacmd.dao.StudentDAO;
+import live.tanujdevops.jpacmd.entity.Student;
+
 @SpringBootApplication
 public class Application {
+
+	private StudentDAO studentDAO;
+
+	@Autowired
+	public Application(StudentDAO studentDAO) {
+		this.studentDAO = studentDAO;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -15,7 +26,16 @@ public class Application {
 	@Bean
 	public CommandLineRunner commandLineRunner(String[] args) {
 		return runner -> {
-			System.out.println("Hello World!");
+			createStudent(studentDAO);
 		};
 	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		System.out.println("Creating new student...");
+		Student haven = new Student("Haven", "Pipoworth", "hpipworth0@furl.net");
+		System.out.println("Saving the student...");
+		studentDAO.save(haven);
+		System.out.println("Saved student's generated id: " + haven.getId());
+	}
+
 }
