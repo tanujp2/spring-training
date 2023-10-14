e43bd91c41c8efbae2e2e9e4dd5d5d989dd0909e
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-14 17:27:43 +0530

    added files: first rest app

---
+++ 19-HelloWorldREST/src/main/java/live/tanujdevops/rest/controller/DemoRestController.java
@@ -0,0 +1,14 @@
+package live.tanujdevops.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //it will enable the REST support
+@RequestMapping("/test")
+public class DemoRestController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
