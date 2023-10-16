7dbeb6974795213057581a955258614379c631c4
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-17 00:17:44 +0530

    used service object instead of doa object

--- _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/controller/EmployeeRestController.java
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/controller/EmployeeRestController.java
@@ -1,7 +1,7 @@
 package live.tanujdevops.rest.controller;
 
-import live.tanujdevops.rest.dao.EmployeeDAO;
 import live.tanujdevops.rest.entity.Employee;
+import live.tanujdevops.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +13,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class EmployeeRestController {
 
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @Autowired
-    public EmployeeRestController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeRestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
-        return employeeDAO.findAll();
+        return employeeService.findAll();
     }
 }
