5e2c761526191a8fcd9fd78a45268766f3e9db13
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-16 16:30:20 +0530

    improved data loading mechanism

--- 20-StudentREST/src/main/java/live/tanujdevops/rest/controller/StudentRestController.java
+++ 20-StudentREST/src/main/java/live/tanujdevops/rest/controller/StudentRestController.java
@@ -1,5 +1,6 @@
 package live.tanujdevops.rest.controller;
 
+import jakarta.annotation.PostConstruct;
 import live.tanujdevops.rest.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
         students.add(new Student("John", "Doe"));
         students.add(new Student("Ramesh", "Pandey"));
         students.add(new Student("Parmesh", "Khatri"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return students;
     }
 }
