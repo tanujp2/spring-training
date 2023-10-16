d8065b42039b3292e7185859a05257d97c9d08a8
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-16 17:29:47 +0530

    added initial files

---
+++ 21-StudentRESTGlobalExceptionHandling/src/main/java/live/tanujdevops/rest/controller/StudentRestController.java
@@ -0,0 +1,56 @@
+package live.tanujdevops.rest.controller;
+
+import jakarta.annotation.PostConstruct;
+import live.tanujdevops.rest.entity.Student;
+import live.tanujdevops.rest.exception.StudentErrorResponse;
+import live.tanujdevops.rest.exception.StudentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.rmi.StubNotFoundException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+        students.add(new Student("John", "Doe"));
+        students.add(new Student("Ramesh", "Pandey"));
+        students.add(new Student("Parmesh", "Khatri"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{id}")
+//    if the path variable name and the method param name are same then there is no need to
+//    specify name attribute in the annotation
+    public Student getStudent(@PathVariable(name = "id") int studentId) {
+        if (studentId < 0 || studentId >= students.size()) {
+            throw new StudentNotFoundException(String.format("Student not found, id = %d", studentId));
+        }
+
+        return students.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException snfe) {
+        StudentErrorResponse error = new StudentErrorResponse(HttpStatus.NOT_FOUND.value(), snfe.getMessage(), new Timestamp(System.currentTimeMillis()));
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
+        StudentErrorResponse error = new StudentErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), new Timestamp(System.currentTimeMillis()));
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ 21-StudentRESTGlobalExceptionHandling/src/main/java/live/tanujdevops/rest/exception/StudentErrorResponse.java
@@ -0,0 +1,42 @@
+package live.tanujdevops.rest.exception;
+
+import java.sql.Timestamp;
+
+public class StudentErrorResponse {
+    private int status;
+    private String message;
+    private Timestamp timestamp;
+
+    public StudentErrorResponse() {
+    }
+
+    public StudentErrorResponse(int status, String message, Timestamp timestamp) {
+        this.status = status;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+}
