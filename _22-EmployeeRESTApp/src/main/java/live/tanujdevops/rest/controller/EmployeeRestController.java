790facf10a488d0b24f52c7c41263be6065dfb82
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-16 23:59:04 +0530

    added rest controller

---
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/controller/EmployeeRestController.java
@@ -0,0 +1,27 @@
+package live.tanujdevops.rest.controller;
+
+import live.tanujdevops.rest.dao.EmployeeDAO;
+import live.tanujdevops.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class EmployeeRestController {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeRestController(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees() {
+        return employeeDAO.findAll();
+    }
+}
