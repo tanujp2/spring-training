11fdb3c0767d76d10ea07bb0533bf69b9136a940
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-16 16:57:43 +0530

    updated finder logic

--- 20-StudentREST/src/main/java/live/tanujdevops/rest/controller/StudentRestController.java
+++ 20-StudentREST/src/main/java/live/tanujdevops/rest/controller/StudentRestController.java
@@ -2,6 +2,7 @@ package live.tanujdevops.rest.controller;
 
 import jakarta.annotation.PostConstruct;
 import live.tanujdevops.rest.entity.Student;
+import live.tanujdevops.rest.exception.StudentNotFoundException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,10 @@ public class StudentRestController {
 //    if the path variable name and the method param name are same then there is no need to
 //    specify name attribute in the annotation
     public Student getStudent(@PathVariable(name = "id") int studentId) {
+        if (studentId < 0 || studentId >= students.size()) {
+            throw new StudentNotFoundException(String.format("Student not found, id = %d", studentId));
+        }
+        
         return students.get(studentId);
     }
 }
