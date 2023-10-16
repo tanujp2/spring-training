80edbd427fcb9cdab431b4b266cdde4f9e551c61
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-17 00:03:33 +0530

    added service layer classes

---
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package live.tanujdevops.rest.service;
+
+import live.tanujdevops.rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> fetchAll();
+}
---
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/service/EmployeeServiceImpl.java
@@ -0,0 +1,24 @@
+package live.tanujdevops.rest.service;
+
+import live.tanujdevops.rest.dao.EmployeeDAO;
+import live.tanujdevops.rest.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO employeeDAO) {
+        this.employeeDAO = employeeDAO;
+    }
+
+    @Override
+    public List<Employee> fetchAll() {
+        return employeeDAO.findAll();
+    }
+}
