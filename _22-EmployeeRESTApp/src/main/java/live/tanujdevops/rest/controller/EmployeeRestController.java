ca9e33d9fe5fdc65ef6afde55c29d5469588d4ba
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-17 16:15:06 +0530

    added controller methods corresponding to the service class

--- _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/controller/EmployeeRestController.java
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/controller/EmployeeRestController.java
@@ -1,11 +1,10 @@
 package live.tanujdevops.rest.controller;
 
+import jakarta.annotation.PostConstruct;
 import live.tanujdevops.rest.entity.Employee;
 import live.tanujdevops.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +22,39 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> getEmployees() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public Object getEmployee(@PathVariable int id) {
+        Employee dbEmployee = employeeService.findById(id);
+        System.out.println(dbEmployee);
+        if (dbEmployee == null) {
+            return "Employee not found, id = " + id;
+        }
+
+        return employeeService.findById(id);
+    }
+
+    @PostMapping("/employees")
+    public Employee saveEmployee(@RequestBody Employee employee) {
+        employee.setId(0); // it will force the employee to be added, instead of update
+        return employeeService.save(employee);
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmplyee(@RequestBody Employee employee) {
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.findById(id);
+
+        if (employee == null) {
+            return "Employee not found, id = " + id;
+        }
+
+        employeeService.deleteById(id);
+        return "Deleted employee, id = " + id;
     }
 }
