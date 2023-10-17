b833fcd15ac11e19001c3305a79ec7f06fffa35b
Tanuj Paraste <tanuj.paraste2@gmail.com>
2023-10-17 15:41:56 +0530

    implemented find, update and delete dao methods in service object

--- _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/service/EmployeeServiceImpl.java
+++ _22-EmployeeRESTApp/src/main/java/live/tanujdevops/rest/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import live.tanujdevops.rest.dao.EmployeeDAO;
 import live.tanujdevops.rest.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -20,5 +21,22 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+    @Override
+    public Employee findById(int id) {
+        return employeeDAO.findById(id);
+    }
+
+    @Override
+    @Transactional
+    public Employee save(Employee employee) {
+        return employeeDAO.save(employee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int id) {
+        employeeDAO.deleteById(id);
     }
 }
